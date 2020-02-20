401b0bf36b1ae2219cc67b0b54e007eb7a66d821
MwasCipher <mwangicollinswaweru@gmail.com>
2020-02-20 10:35:54 +0300

    Database Operations

--- includes/DBOperations.php
+++ includes/DBOperations.php
@@ -3,13 +3,25 @@
 
         private $con;
 
-        function __construct()
-        {
+        function __construct(){
             require_once dirname(__FILE__).'/DBConnection.php';
 
             $db = new DBConnection();
 
             $this->con = $db->connect();
+        }
+
+        function createUser($username, $pword, $email){
+            $password = md5($pword);
+            $statement = $this->con->prepare("INSERT INTO 'users' ('id', 'username', 'password', 'email')
+                                            VALUES (NULL , ?, ?, ?); ");
+            $statement->bind_param("sss", $username, $password, $email);
+
+            if ($statement->execute()){
+                return true;
+            }else{
+                return false;
+            }
         }
 
     }
